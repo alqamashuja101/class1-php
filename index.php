834675dca11723ef4f1ae0be8cb9734c1b467ec4
Alqama <Alqama@aptechnn2.edu>
2022-07-29 11:04:59 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-Class One</title>
+</head>
+<body>
+    <?php
+        $a = 25;
+        $b = 3;
+        if($a % 2 == 0){
+            echo "$a is EVEN ".$a % 2;
+        }
+        else{
+            echo "$a is ODD".$a % 2;
+        }
+    ?>
+</body>
+</html>
+
+<?php
+        $abc = "<br> Ali";
+        echo $abc
+    ?>
